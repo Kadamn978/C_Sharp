277cebc4814e4a8a7ffb5c9ac9c6632901991bb3
Nilesh Kadam <101574501+Kadamn978@users.noreply.github.com>
2022-03-24 19:47:17 +0530

    Add files via upload

---
+++ TSystem_InheritanceAndPloymorphism/TSystem_InheritanceAndPloymorphism/Calculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_InheritanceAndPloymorphism
+{
+    internal class Calculation
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Addition(int a, int b, int c)
+        {
+            return a + b +c ;
+        }
+        public double Addition(int a, int b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ TSystem_InheritanceAndPloymorphism/TSystem_InheritanceAndPloymorphism/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_InheritanceAndPloymorphism
+{
+    public class Employee
+    {
+
+        static int count;
+        protected int id;
+        protected string name;
+        protected double basesalary, hra, da, pf, grosssalary;
+        public Employee()
+        {
+
+            name = "";
+            basesalary = 0.0;
+        }
+        public Employee( string name, double basesalary)
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.basesalary = basesalary;
+        }
+        public virtual void salarycalculate()
+        {
+            hra = basesalary * (0.40);
+            da = basesalary * (0.20);
+            pf = basesalary * (0.12);
+            grosssalary = basesalary + hra + da - pf;
+        }
+        public static int GetCount()
+        {
+            return count;
+        }
+        public override string ToString()
+        {
+            return " Emp ID  " + id + ", Emp Name " + name + ", Emp Salary " + grosssalary;
+        }
+    }
+}
